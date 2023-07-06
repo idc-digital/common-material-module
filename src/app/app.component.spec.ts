e85037b6ff63a120c2c79ba49188eae23a68bc48
Bakht Munir <bakhtmuniruet@gmail.com>
2023-07-06 16:01:05 +0400

    common material issue done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('common-material-module-angular');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('common-material-module-angular app is running!');
-  });
+
 });
